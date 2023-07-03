5a4c178927258739ab124d41dc4403bcce486ad9
IJACHI <ijachiijachi@live.com>
2023-07-03 06:13:03 -0700

    C hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - The main Entry point
+  *Return: always return 0 (success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
